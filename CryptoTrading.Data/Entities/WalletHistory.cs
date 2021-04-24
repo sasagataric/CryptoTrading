ecaf04f66f9a08dd32dd368943dc2949da016b68
Sasa Gataric <sasagataric@gmail.com>
2021-04-24 03:30:31 +0200

    Created DBContext and added migrations

---
+++ CryptoTrading.Data/Entities/WalletHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTrading.Data.Entities
+{
+    public class WalletHistory
+    {
+        public Guid Id { get; set; }
+        public string CoinId { get; set; }
+        public Coin Coin { get; set; }
+        public Guid WalletId { get; set; }
+        public Wallet Wallet { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public double amount { get; set; }
+        public double CoinPrice { get; set; }
+    }
+}
